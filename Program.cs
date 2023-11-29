f9f4af33723200e3777cddf5e9645c464b74fff0
NazrenG <nezrin.qu@gmail.com>
2023-11-29 17:39:42 +0400

    factory method

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Factory
+{
+    public interface IButton
+    {
+        public void render();
+        public void onClick();
+    }
+
+    public class WindowsButton : IButton
+    {
+        public void onClick()
+        {
+            //do work
+        }
+
+        public void render()
+        {
+            //do work
+        }
+    }
+
+    public class HTMLButton : IButton
+    {
+        public void onClick()
+        {
+            //do work
+        }
+
+        public void render()
+        {
+            //do work
+        }
+    }
+
+    public abstract class Dialog
+    {
+        public IButton? okButton;
+
+
+        public abstract IButton createButton();
+
+        public void render()
+        {
+            okButton = createButton();
+            okButton.onClick();
+            okButton.render();
+
+        }
+    }
+
+    public class WindowsDialog : Dialog
+    {
+        public override IButton createButton()
+        {
+            return new WindowsButton();
+        }
+    }
+
+    public class WebDialog : Dialog
+    {
+        public override IButton createButton()
+        {
+            return new HTMLButton();
+        }
+    }
+}
